05ce773a81743c1b6d56e3b69f10452b94e2d6ce
Vitor <vitormateushd123@gmail.com>
2025-10-03 06:42:11 -0300

    Update conexao.py

--- Atividade/conexao.py
+++ Atividade/conexao.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def criar_conexao():
     try:
-        conexao = sqlite3.connect("banco.db")  # Cria um arquivo local chamado meu_banco.db
+        conexao = sqlite3.connect("banco.db")  # Cria um arquivo local chamado banco.db
         return conexao
     except sqlite3.Error as e:
         print(f"Erro ao conectar ao SQLite: {e}")
